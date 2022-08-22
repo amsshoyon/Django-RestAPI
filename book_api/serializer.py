f8c5fdcfd23bc55e009517b2794066b3b85e53f2
Abdullah Al Mamun <amsshoyon@gmail.com>
2022-08-22 17:46:37 +0600

    Book api: Get & Post

---
+++ book_api/serializer.py
@@ -0,0 +1,14 @@
+from turtle import title
+from rest_framework import serializers
+from django.forms import IntegerField
+from book_api.models import Book
+
+class BookSerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
+    title = serializers.CharField()
+    number_of_pages = serializers.IntegerField()
+    publish_date = serializers.DateField()
+    quantity = serializers.IntegerField()
+    
+    def create(self, data):
+        return Book.objects.create(**data)
